10239f7913c044386e2f4b1da285c1676e8fcc65
Dishadinesh85 <dishapoojary85@gmail.com>
2026-05-23 11:37:04 +0530

    Add Retail Sales Analytics Dashboard with Streamlit

--- deployment/app.py
+++ deployment/app.py
@@ -1,1 +1,25 @@
+import streamlit as st
+import pandas as pd
 
+st.title("Retail Sales Analytics Dashboard")
+df = pd.read_csv("data/cleaned_data.csv")
+
+st.subheader("Dataset Preview")
+st.write(df.head())
+
+category = st.selectbox(
+    "Select Product Category",
+    df['Product Category'].unique()
+)
+
+filtered = df[
+    df['Product Category'] == category
+]
+
+st.subheader("Sales by Month")
+
+monthly = filtered.groupby(
+    'Month'
+)['Total Amount'].sum()
+
+st.line_chart(monthly)
